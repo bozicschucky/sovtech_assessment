2d4b7c53dfbcd48540af65c65ab6d33356591ca6
chucky <charles.ssekitto@cobblestoneenergy.com>
2022-04-06 03:27:59 +0300

    add the person details logic

---
+++ client/src/components/PeopleDetails.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GET_PERSON_DETAILS_QUERY } from "../queries/personDetails";
+
+export default function PeopleDetails() {
+  const { name } = useParams();
+  const { loading, error, data } = useQuery(GET_PERSON_DETAILS_QUERY, {
+    variables: { name },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const personDetails = data?.search?.results[0];
+  return (
+    <div className="card">
+      <h3>User Info</h3>
+      <p>Name: {personDetails.name}</p>
+      <p>Gender: {personDetails.gender}</p>
+      <p>Height: {personDetails.height}</p>
+      <p>mass: {personDetails.mass}</p>
+    </div>
+  );
+}
---
+++ client/src/queries/personDetails.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const GET_PERSON_DETAILS_QUERY = gql`
+  query Search($name: String!) {
+    search(name: $name) {
+      count
+      results {
+        name
+        gender
+        height
+        mass
+        gender
+      }
+    }
+  }
+`;
